ee11481febb70dd699275201e6050a7175405ccc
vanchien08 <nguyenchien112azs@gmail.com>
2025-08-12 20:33:47 +0700

    api update status ticket

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -2,5 +2,8 @@ import axios from "../axios";
 const handleGetTicketByUserId = (id) => {
   return axios.get(`/api/admin/get-ticket-by-idUser?id=${id}`);
 };
+const handleUpdateTicketStatus = (id, status) => {
+  return axios.put(`/api/admin/update-ticket-status?id=${id}&status=${status}`);
+};
 
-export { handleGetTicketByUserId };
+export { handleGetTicketByUserId, handleUpdateTicketStatus };
